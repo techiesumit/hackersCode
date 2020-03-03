8a2b194063e4b18b92d5d5f42e2a4f360240fdf4
Sumit Chawla <techiesumit@outlook.com>
2020-03-02 22:18:33 -0600

    add

--- LeetCode/Datastructure/DecompressesArray.java
+++ LeetCode/Datastructure/DecompressesArray.java
@@ -1,1 +1,27 @@
+import java.util.List;
+import java.util.ArrayList;
+public class DecompressesArray {
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> outList=new ArrayList<Integer>();
+        if(nums.length >=2  && nums.length <=100 && (nums.length % 2 ==0)) {
+            int frequency=0;
+            int value=0;
+            for (int i = 0; i < nums.length ; i++) {
+                frequency=nums[i];
+                i++;
+                value=nums[i];
+                if(frequency > 0 && value >0){
+                    for (int j = 0; j < frequency; j++) {
+                        outList.add(value);
+                    }
+                }
 
+            }
+        }else
+        {
+            System.out.println("Array lenght is not in the range ");
+        }
+       
+        return outList.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
---
+++ LeetCode/Datastructure/SampleTest.java
@@ -0,0 +1,6 @@
+class SampleTest
+{
+public static void main(String[] args){
+System.out.println("Hello Testing App");
+}
+}
